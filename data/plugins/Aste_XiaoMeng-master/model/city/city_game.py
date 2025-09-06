7360433ec9ff9823fc8120fb2542affb387acdcc
HANG-XM <h2006z@163.com>
2025-09-06 15:20:10 +0800

    city_game.update_notice功能优化
    添加更新公告显示所有更新ID

--- data/plugins/Aste_XiaoMeng-master/model/city/city_game.py
+++ data/plugins/Aste_XiaoMeng-master/model/city/city_game.py
@@ -1011,16 +1011,36 @@ BUG少了~游戏也更加顺畅了~
         if update:
             return update['content']
         return "未找到该日期的更新公告"
+    def get_all_update_ids(self):
+        """获取所有更新公告的ID列表"""
+        return list(self.updates.keys())
     
 
 def update_notice(msg:str):
-        
+   # 创建全局实例
+    update_manager = GameUpdateManager()
+    
+    # 检查是否请求所有ID列表
+    if msg == "更新公告":
+        # 获取所有ID
+        all_ids = update_manager.get_all_update_ids()
+        # 每四个ID一行进行格式化
+        formatted_ids = []
+        for i in range(0, len(all_ids), 4):
+            # 获取当前行的ID（最多4个）
+            line_ids = all_ids[i:i+4]
+            # 将这一行的ID连接成一个字符串
+            line = ", ".join(line_ids)
+            formatted_ids.append(line)
+        # 将所有行用换行符连接
+        result = "可用的更新公告ID:\n" + "\n".join(formatted_ids)
+        return result
+    else:
+        # 提取特定的ID
+        notice_id = msg.replace("更新公告 ", "")
+        # 获取并返回特定ID的内容
+        notice_detail = update_manager.get_update_by_date(notice_id)
+        return notice_detail
 
-        
-        notice_id = msg.replace("更新公告 ","")
-
-        # 创建全局实例
-        update_manager = GameUpdateManager()
-
-        notice_datail = update_manager.get_update_by_date(notice_id)
-        return notice_datail
+def game_menu():
+    return """逃跑吧少年游戏助手\n1.更新公告\n2.兑换代码"""
